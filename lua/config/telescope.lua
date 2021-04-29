c493bcde4c1f318f22bc3ac250061f25889b8ce3
shun-sfoo <ganymede0915@gmail.com>
2021-04-29 22:26:09 +0800

    telescope buffers

--- lua/config/telescope.lua
+++ lua/config/telescope.lua
@@ -3,6 +3,8 @@ local map = utils.map
 
 map("n", "<leader>ff", "<cmd>Telescope find_files<cr>")
 map("n", "<leader>fg", "<cmd>Telescope live_grep<cr>")
-map("n", "<leader>fb", "<cmd>Telescope buffers<cr>")
+-- can't show all buffer issues
+-- https://github.com/nvim-telescope/telescope.nvim/issues/368
+map("n", "<leader>fb", "<cmd>Telescope buffers show_all_buffers=true<cr>")
 map("n", "<leader>fh", "<cmd>Telescope oldfiles<cr>")
 map("n", "<leader>ft", "<cmd>Telescope help_tags<cr>")

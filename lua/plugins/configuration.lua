6e726ed144c2b4cebbfac88a62e88dd479b155ef
shun-sfoo <ganymede0915@gmail.com>
2023-11-26 22:55:36 +0800

    plugin: yuck

--- lua/plugins/configuration.lua
+++ lua/plugins/configuration.lua
@@ -2,9 +2,34 @@ return {
   {
     'folke/tokyonight.nvim',
     lazy = true,
-    opts = { style = 'storm' },
+    opts = {
+      style = 'storm',
+      transparent = true,
+      styles = {
+        sidebars = 'transparent',
+        floats = 'transparent',
+      },
+    },
   },
   {
     { 'Pocco81/auto-save.nvim', event = 'BufEnter', opts = {} },
+  },
+  {
+    'nvim-treesitter/nvim-treesitter',
+    opts = {
+      ensure_installed = {
+        'scheme',
+      },
+    },
+  },
+  {
+    'elkowar/yuck.vim',
+    ft = 'yuck',
+    init = function()
+      vim.g.yuck_align_multiline_strings = 1
+      vim.g.yuck_align_subforms = 1
+      vim.g.yuck_align_keywords = 0
+      -- vim.g.yuck_lisp_indentation = 1
+    end,
   },
 }
